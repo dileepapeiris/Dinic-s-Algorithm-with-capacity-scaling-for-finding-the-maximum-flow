4ba226b23bfa38119142ff8ea82824a0f69662e9
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-07-31 23:30:12 +0530

    Add main method and documentation for Dinic's algorithm

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -1,3 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    
+
+     /**
+     * Main method to run the Dinic's algorithm with capacity scaling.
+     * It reads a flow network from a file, computes the maximum flow,
+     * and optionally displays detailed flow information.
+     *
+     * @param args Command line arguments
+     * @throws IOException If there is an error reading the file
+     * @throws IllegalArgumentException If the input is invalid
+     * @throws Exception For any other unexpected errors
+     
+     */
+    public static void main(String[] args) {
+        System.out.println("Network Flow Algorithm ");
+        Scanner scanner = new Scanner(System.in);
+
 }
